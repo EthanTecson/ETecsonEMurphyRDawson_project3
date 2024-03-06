c2b8379322d79df21b59c66eb8607ca509899428
Ethan Tecson <ethantecson@gmail.com>
2024-03-05 19:37:00 -0500

    Change in header file. Created an array of pointers

--- hash_table.h
+++ hash_table.h
@@ -23,11 +23,6 @@ private:
     int slots;
     T** array;
 
-    // See if this will work as a pointer
-    struct Node {
-        Node *next;
-    };
-
 public:
     HashTable(int numSlots);                       // After looking at test cases, I think HashTable always needs to be initiated with a number of slots
     HashTable(const HashTable<T> &myHashTable); // Copy Constructor
@@ -36,7 +31,6 @@ public:
     void remove(const T k);                     // Removes element of value k (we are allowed to assume that all keys are distinct)
     bool member(const T d, const T k) const;    // check to see if an element with data d and key k is apart of the HashTable
     string to_string() const;
-    bool operator==(const HashTable<T>& other) const;
 };
 
 #endif
