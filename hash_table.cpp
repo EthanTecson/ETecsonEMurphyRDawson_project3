b2e77c123da33e50b25160a23b775e159c9d07c8
RyanDaawson <dawson_r1@denison.edu>
2024-03-06 21:49:23 -0500

    Comments

--- hash_table.cpp
+++ hash_table.cpp
@@ -61,8 +61,8 @@ Element<T>::Element(const T &d, const T &k)
 template <class T>
 Element<T>::Element(const Element<T> &myElement)
 {
-    data = myElement.data;
-    key = myElement.key;
+    data = myElement.data; // Set data to the given Element's data
+    key = myElement.key;   // Set key to the given Element's key
 }
 
 /**
@@ -92,7 +92,7 @@ Element<T>::~Element()
 template <class T>
 int Element<T>::get_key()
 {
-    return key;
+    return key; // Return the key value of the element
 }
 
 /**
@@ -107,10 +107,8 @@ int Element<T>::get_key()
 template <class T>
 T Element<T>::get_data()
 {
-    return data;
+    return data; // Return the data value of the element
 }
-
-
 
 //==================================
 // Hash Table Class
@@ -128,8 +126,8 @@ T Element<T>::get_data()
 template <class T>
 HashTable<T>::HashTable(int numSlots)
 {
-    slots = numSlots; 
-    table = new list<Element<T>>[slots]; 
+    slots = numSlots;                    // Set slots to the given number of slots
+    table = new list<Element<T>>[slots]; // Create a new array of lists of elements
 }
 
 // /**
@@ -144,9 +142,27 @@ HashTable<T>::HashTable(int numSlots)
 template <class T>
 HashTable<T>::HashTable(const HashTable<T> &myHashTable)
 {
-    // Write code here
-}
+    slot = myHashTable.slots;            // Set slots to the given number of slots
+    table = new list<Element<T>>[slots]; // Create a new array of lists of elements
 
+    for (int i = 0; i < slots; ++i) // Copy the elements from the given hash table to the new hash table
+    {
+        if (myHashTable.table[i].empty()) // If the list is empty, move to the next list
+        {
+            continue; // Move to the next list
+        }
+
+        auto it = myHashTable.table[i].begin(); // iterator to the beginning of the list
+        auto end = myHashTable.table[i].end();  // end of the list
+
+        while (it != end) // Copy the elements from the given hash table to the new hash table
+        {
+            Element<T> e(it->get_data(), it->get_key()); // Create a new element with the same data and key as the given element
+            table[i].push_front(e);                      // Insert the element at the front of the list
+            ++it;                                        // Move to the next element in the list
+        }
+    }
+}
 
 /**
  * @brief Deconstructor Class
@@ -158,61 +174,65 @@ HashTable<T>::HashTable(const HashTable<T> &myHashTable)
  * @returns none
  */
 template <class T>
-HashTable<T>::~HashTable(void) {
+HashTable<T>::~HashTable(void)
+{
     delete[] table; // Deallocate memory for the array
 }
 
 /**
  * @brief insert Class
  *
- * 
  *
- * @note Pre-Condition: 
- * @note Post-Condition: 
+ *
+ * @note Pre-Condition:
+ * @note Post-Condition:
  * @returns none
  */
 template <class T>
 void HashTable<T>::insert(const T d, const T k)
 {
-    Element<T> e(d,k);
+    Element<T> e(d, k); // Create an element with the given data and key
     int position = k % slots;
     // Check if hash table is initiated to 0 first or else we get segmentation error
-    if (slots == 0){
+    if (slots == 0)
+    {
         return;
     }
     // Check if inserted element is a member
-    else {
-        int position = k % slots;
-        auto it = table[position].begin();
-        auto end = table[position].end();
+    else
+    {
+        int position = k % slots;          // Get the position of the element in the table
+        auto it = table[position].begin(); // iterator to the beginning of the list
+        auto end = table[position].end();  // end of the list
 
-        while (it != end)
+        while (it != end) // Check if the element is already in the table
         {
-            if (it->get_data() == d && it->get_key() == k)
+            if (it->get_data() == d && it->get_key() == k) // If the element is already in the table, don't insert it again
             {
                 return;
             }
-            ++it;
+            ++it; // Move to the next element in the list
         }
     }
     // Otherwise insert
-    table[position].push_front(e);
+    table[position].push_front(e); // Insert the element at the front of the list
 }
 
 /**
  * @brief remove Class
  *
- * 
  *
- * @note Pre-Condition: 
- * @note Post-Condition: 
- * @returns 
+ *
+ * @note Pre-Condition:
+ * @note Post-Condition:
+ * @returns
  */
 template <class T>
 void HashTable<T>::remove(const T k)
 {
     // Check if hash table is initiated to 0
-    if (slots == 0){
+    if (slots == 0)
+    {
         return;
     }
     // Check if element to be removed is in table
@@ -229,34 +249,36 @@ void HashTable<T>::remove(const T k)
             table[position].erase(it);
             return;
         }
-        else 
+        else
         {
             ++it;
         }
     }
-}              
+}
 
 /**
  * @brief member Class
- * 
- * 
- * 
- * @note Pre-Condition: 
- * @note Post-Condition: 
+ *
+ *
+ *
+ * @note Pre-Condition:
+ * @note Post-Condition:
  * @returns none
  */
 template <class T>
 bool HashTable<T>::member(const T d, const T k) const
 {
-    int position = k % slots;
-    auto it = table[position].begin();
-    auto end = table[position].end();
+    int position = k % slots;          // Get the position of the element in the table
+    auto it = table[position].begin(); // iterator to the beginning of the list
+    auto end = table[position].end();  // end of the list
 
-    if (table[position].empty()){
-        return false;
+    if (table[position].empty()) // If the list is empty, the element is not in the table
+    {
+        return false; // Return false
     }
-    else {
-        while (it != end)
+    else
+    {
+        while (it != end) // Check if the element is in the table
         {
             if (it->get_data() == d && it->get_key() == k)
             {
@@ -268,32 +290,33 @@ bool HashTable<T>::member(const T d, const T k) const
     }
 }
 
-
 /**
  * @brief to_string Class
- * 
- * 
- * 
- * @note Pre-Condition: 
- * @note Post-Condition: 
- * @returns 
+ *
+ *
+ *
+ * @note Pre-Condition:
+ * @note Post-Condition:
+ * @returns
  */
 template <class T>
 string HashTable<T>::to_string() const
 {
     stringstream stream;
     int slots_counter = 0;
-    for (int i = 0; i < slots; ++i){
+    for (int i = 0; i < slots; ++i)
+    {
         auto it = table[i].begin();
         auto end = table[i].end();
-        stream << i << ":" << " "; 
+        stream << i << ":"
+               << " ";
         while (it != end)
         {
             stream << "(" << it->get_data() << "," << it->get_key() << ")";
             stream << " ";
             ++it;
         }
-        stream << "\n"; 
+        stream << "\n";
     }
     return stream.str();
 }
