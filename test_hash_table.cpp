14a10b677d2c987d416b8ef4bc28c0b1f8ac8a7e
Ethan Tecson <ethantecson@gmail.com>
2024-03-06 20:52:39 -0500

    All methods done except copy constructor. hash_table.h updated correctly. test file ready to go. Comments needed

--- test_hash_table.cpp
+++ test_hash_table.cpp
@@ -28,17 +28,10 @@ void test_get_key()
         {
             cout << "Incorrect result from get key. Expected 6 but got : " << e.get_key() << endl;
         }
-        // Testing copy constructor
         Element<int> f(e); 
         if (e.get_key() != f.get_key())
         {
             cout << "Incorrect result from copy constructor and get key. Expected 6 but got : " << f.get_key() << endl;
-        }
-        // Testing with strings
-        Element<string> strE("hello", 5);
-        if (strE.get_key() != 5)
-        {
-            cout << "Incorrect result from get key. Expected 5 but got : " <<  strE.get_key() << endl;
         }
     }
     catch (exception &e)
@@ -61,11 +54,6 @@ void test_get_data()
         {
             cout << "Incorrect result from get data. Expected 10 but got : " << e.get_data() << endl;
         }
-        Element<string> strE("hello", 5);
-        if (strE.get_data() != "hello")
-        {
-            cout << "Incorrect result from get data. Expected hello but got : " <<  strE.get_data() << endl;
-        }
     }
     catch (exception &e)
     {
@@ -73,131 +61,132 @@ void test_get_data()
     }
 }
 
-// void test_insert()
-// {
-//     try
-//     {
-//         HashTable<int> empty_ht(0);
-//         empty_ht.insert(10, 6);
-//         if (empty_ht.to_string() != "")
-//         {
-//             cout << "Incorrect result of inserting into table. Expected and empty string But got\n\n"
-//                  << empty_ht.to_string() << endl;
-//         }
-//     }
-//     catch (exception &e)
-//     {
-//         cout << "Error caused by trying to insert into empty table : " << e.what() << endl;
-//     }
-//     try
-//     {
-//         HashTable<int> ht(5);
-//         ht.insert(10, 6);
-//         if (ht.to_string() != "0: \n1: (10,6) \n2: \n3: \n4: \n")
-//         {
-//             cout << "Incorrect result of inserting into table. Expected\n\n0: \n1: (10,6) \n2: \n3: \n4: \n\nBut got\n\n"
-//                  << ht.to_string() << endl;
-//         }
-//         ht.insert(1, 21);
-//         if (ht.to_string() != "0: \n1: (1,21) (10,6) \n2: \n3: \n4: \n")
-//         {
-//             cout << "Incorrect result of inserting into table" << endl;
-//         }
-//     }
-//     catch (exception &e)
-//     {
-//         cerr << "Error inserting into non-empty table : " << e.what() << endl;
-//     }
-// }
 
-// void test_remove()
-// {
-//     try
-//     {
-//         HashTable<int> empty_ht(0);
-//         empty_ht.remove(6);
-//         if (empty_ht.to_string() != "")
-//         {
-//             cout << "Incorrect result of removing from empty table. Expected and empty string But got\n\n"
-//                  << empty_ht.to_string() << endl;
-//         }
-//     }
-//     catch (exception &e)
-//     {
-//         cout << "Error caused by trying to remove from empty table : " << e.what() << endl;
-//     }
-//     try
-//     {
-//         HashTable<int> ht(5);
-//         ht.insert(10, 6);
-//         ht.remove(5);
-//         if (ht.to_string() != "0: \n1: (10,6) \n2: \n3: \n4: \n")
-//         {
-//             cout << "Incorrect result of removing non-member from table. Expected\n\n0: \n1: (10,6) \n2: \n3: \n4: \n\nBut got\n\n"
-//                  << ht.to_string() << endl;
-//         }
-//     }
-//     catch (exception &e)
-//     {
-//         cerr << "Error removing non-member from table : " << e.what() << endl;
-//     }
-//     try
-//     {
-//         HashTable<int> ht2(5);
-//         ht2.insert(10, 6);
-//         ht2.remove(6);
-//         if (ht2.to_string() != "0: \n1: \n2: \n3: \n4: \n")
-//         {
-//             cout << "Incorrect result removing member from table. Expected\n\n0: \n1: \n2: \n3: \n4: \n\nBut got\n\n"
-//                  << ht2.to_string() << endl;
-//         }
-//     }
-//     catch (exception &e)
-//     {
-//         cerr << "Error removing member from table : " << e.what() << endl;
-//     }
-// }
+void test_insert()
+{
+    try
+    {
+        HashTable<int> empty_ht(0);
+        empty_ht.insert(10, 6);
+        if (empty_ht.to_string() != "")
+        {
+            cout << "Incorrect result of inserting into table. Expected and empty string But got\n\n"
+                 << empty_ht.to_string() << endl;
+        }
+    }
+    catch (exception &e)
+    {
+        cout << "Error caused by trying to insert into empty table : " << e.what() << endl;
+    }
+    try
+    {
+        HashTable<int> ht(5);
+        ht.insert(10, 6);
+        if (ht.to_string() != "0: \n1: (10,6) \n2: \n3: \n4: \n")
+        {
+            cout << "Incorrect result of inserting into table. Expected\n\n0: \n1: (10,6) \n2: \n3: \n4: \n\nBut got\n\n"
+                 << ht.to_string() << endl;
+        }
+        ht.insert(1, 21);
+        if (ht.to_string() != "0: \n1: (1,21) (10,6) \n2: \n3: \n4: \n")
+        {
+            cout << "Incorrect result of inserting into table" << endl;
+        }
+    }
+    catch (exception &e)
+    {
+        cerr << "Error inserting into non-empty table : " << e.what() << endl;
+    }
+}
 
-// void test_member()
-// {
-//     try
-//     {
-//         HashTable<int> empty_ht(0);
-//         if (empty_ht.member(10, 6))
-//         {
-//             cout << "Incorrect membership in empty table" << endl;
-//         }
-//     }
-//     catch (exception &e)
-//     {
-//         cout << "Error caused by trying to determine membership in empty table : " << e.what() << endl;
-//     }
-//     try
-//     {
-//         HashTable<int> ht(5);
-//         ht.insert(10, 6);
-//         if (ht.member(11, 6))
-//         {
-//             cout << "Incorrect membership in table" << endl;
-//         }
-//         if (!ht.member(10, 6))
-//         {
-//             cout << "Incorrect non-membership in table" << endl;
-//         }
-//     }
-//     catch (exception &e)
-//     {
-//         cerr << "Error determining membership from table : " << e.what() << endl;
-//     }
-// }
+void test_remove()
+{
+    try
+    {
+        HashTable<int> empty_ht(0);
+        empty_ht.remove(6);
+        if (empty_ht.to_string() != "")
+        {
+            cout << "Incorrect result of removing from empty table. Expected and empty string But got\n\n"
+                 << empty_ht.to_string() << endl;
+        }
+    }
+    catch (exception &e)
+    {
+        cout << "Error caused by trying to remove from empty table : " << e.what() << endl;
+    }
+    try
+    {
+        HashTable<int> ht(5);
+        ht.insert(10, 6);
+        ht.remove(5);
+        if (ht.to_string() != "0: \n1: (10,6) \n2: \n3: \n4: \n")
+        {
+            cout << "Incorrect result of removing non-member from table. Expected\n\n0: \n1: (10,6) \n2: \n3: \n4: \n\nBut got\n\n"
+                 << ht.to_string() << endl;
+        }
+    }
+    catch (exception &e)
+    {
+        cerr << "Error removing non-member from table : " << e.what() << endl;
+    }
+    try
+    {
+        HashTable<int> ht2(5);
+        ht2.insert(10, 6);
+        ht2.remove(6);
+        if (ht2.to_string() != "0: \n1: \n2: \n3: \n4: \n")
+        {
+            cout << "Incorrect result removing member from table. Expected\n\n0: \n1: \n2: \n3: \n4: \n\nBut got\n\n"
+                 << ht2.to_string() << endl;
+        }
+    }
+    catch (exception &e)
+    {
+        cerr << "Error removing member from table : " << e.what() << endl;
+    }
+}
+
+void test_member()
+{
+    try
+    {
+        HashTable<int> empty_ht(0);
+        if (empty_ht.member(10, 6))
+        {
+            cout << "Incorrect membership in empty table" << endl;
+        }
+    }
+    catch (exception &e)
+    {
+        cout << "Error caused by trying to determine membership in empty table : " << e.what() << endl;
+    }
+    try
+    {
+        HashTable<int> ht(5);
+        ht.insert(10, 6);
+        if (ht.member(11, 6))
+        {
+            cout << "Incorrect membership in table" << endl;
+        }
+        if (!ht.member(10, 6))
+        {
+            cout << "Incorrect non-membership in table" << endl;
+        }
+    }
+    catch (exception &e)
+    {
+        cerr << "Error determining membership from table : " << e.what() << endl;
+    }
+}
+
 int main()
 {
-
     test_get_key();
-    //test_get_data();
-    // test_insert();
-    // test_remove();
-    // test_member();
+    test_get_data();
+    test_insert();
+    test_remove();
+    test_member();
 
     cout << "Testing completed" << endl;
 
